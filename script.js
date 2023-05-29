46fb137c55ef3fa116b809e247eb59c2826036e5
wwz07 <iwbzul.backup1@gmail.com>
2023-05-29 14:16:03 +0700

    basic pagination mechanics

---
+++ script.js
@@ -0,0 +1,19 @@
+const pagesData = {
+    1: {
+        id: 1,
+        header: 'Contact details',
+        subheader: 'Lorem ipsum dolor sit amet consectetur adipisc.',
+    },
+    2: {
+        id: 2,
+        header: 'Our services',
+        subheader: 'Please select which service you are interested in.',
+    },
+    3: {
+        id: 3,
+        header: 'What is your project budget?',
+        subheader: 'Please select the project budget you have in mind.',
+    },
+}
+
+// Export the pagesData object
