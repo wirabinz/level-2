9f273a6fa8d239ef0cdf715c8fa93ca245eed48c
wirazulka <iwbzul.backup1@gmail.com>
2023-06-01 21:56:44 +0700

    struggling in page 1

--- script.js
+++ script.js
@@ -14,5 +14,10 @@ const pagesData = {
         header: 'What is your project budget?',
         subheader: 'Please select the project budget you have in mind.',
     },
+    4: {
+        id: 4,
+        header: 'Submit your quote request',
+        subheader: 'Please review all the information you previously typed in the past step, and if all is okay, submit your message to receive a project quote in 24-48 hours.',
+    },
 }
 
