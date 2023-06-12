a22652b76d30d6c5b2209d99bfae0ee569f323b9
wwz07 <iwbzul.backup1@gmail.com>
2023-06-12 13:15:13 +0700

    revision done

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const pagesDatax = {
-    1: {
-        id: 1,
-        header: 'Contact details',
-        subheader: 'Lorem ipsum dolor sit amet consectetur adipisc.',
-    },
-    2: {
-        id: 2,
-        header: 'Our services',
-        subheader: 'Please select which service you are interested in.',
-    },
-    3: {
-        id: 3,
-        header: 'What is your project budget?',
-        subheader: 'Please select the project budget you have in mind.',
-    },
-    4: {
-        id: 4,
-        header: 'Submit your quote request',
-        subheader: 'Please review all the information you previously typed in the past step, and if all is okay, submit your message to receive a project quote in 24-48 hours.',
-    },
-}
+// const pagesDatax = {
+    // 1: {
+        // id: 1,
+        // header: 'Contact details',
+        // subheader: 'Lorem ipsum dolor sit amet consectetur adipisc.',
+    // },
+    // 2: {
+        // id: 2,
+        // header: 'Our services',
+        // subheader: 'Please select which service you are interested in.',
+    // },
+    // 3: {
+        // id: 3,
+        // header: 'What is your project budget?',
+        // subheader: 'Please select the project budget you have in mind.',
+    // },
+    // 4: {
+        // id: 4,
+        // header: 'Submit your quote request',
+        // subheader: 'Please review all the information you previously typed in the past step, and if all is okay, submit your message to receive a project quote in 24-48 hours.',
+    // },
+// }
 
 const pagesData = [
     {
