54a81dd16da9d0439ae549b719dfaac2bd186149
wirazulka <iwbzul.backup1@gmail.com>
2023-06-25 21:25:48 +0700

    restructuring the code

--- script.js
+++ script.js
@@ -22,27 +22,88 @@
 // }
 
 const pagesData = [
-    {
-      id: 1,
-      header: 'Contact details',
-      subheader: 'Lorem ipsum dolor sit amet consectetur adipisc.',
-    },
-    {
-      id: 2,
-      header: 'Our services',
-      subheader: 'Please select which service you are interested in.',
-    },
-    {
-      id: 3,
-      header: 'What is your project budget?',
-      subheader: 'Please select the project budget you have in mind.',
-    },
-    {
-      id: 4,
-      header: 'Submit your quote request',
-      subheader:
-        'Please review all the information you previously typed in the past step, and if all is okay, submit your message to receive a project quote in 24-48 hours.',
-    },
-  ];
+  {
+    pagenumber: 1,
+    header: 'Contact details',
+    subheader: 'Lorem ipsum dolor sit amet consectetur adipisc.',
+  },
+  {
+    pagenumber: 2,
+    header: 'Our services',
+    subheader: 'Please select which service you are interested in.',
+  },
+  {
+    pagenumber: 3,
+    header: 'What is your project budget?',
+    subheader: 'Please select the project budget you have in mind.',
+  },
+  {
+    pagenumber: 4,
+    header: 'Submit your quote request',
+    subheader:
+      'Please review all the information you previously typed in the past step, and if all is okay, submit your message to receive a project quote in 24-48 hours.',
+  },
+];
+
+const inputFormData=[
+  {
+    title:'Name',
+    icon:'user',
+    errormessage:'Invalid name.',
+    isValid:true,
+  },
+  {
+    title:'Email',
+    icon:'mail',
+    errormessage:'Invalid email format.',
+    isValid:true,
+  },
+  {
+    title:'Phone Number',
+    icon:'mobile',
+    errormessage:'Invalid phone number.',
+    isValid:true,
+  },
+  {
+    title:'Company',
+    icon:'building',
+    errormessage:'Invalid company.',
+    isValid:true,
+  },
+];
+
+const serviceFormData=[
+  {
+    title:'Development',
+    icon:'development',
+  },
+  {
+    title:'Web Design',
+    icon:'webdesign',
+  },
+  {
+    title:'Marketing',
+    icon:'marketing',
+  },
+  {
+    title:'Other',
+    icon:'other',
+  },
+];
   
+const budgetFormData=[
+  {
+    budget:'$5000-$10000',
+  },
+  {
+    budget:'$10.000-$20.000',
+  },
+  {
+    budget:'$20.000-$50.000',
+  },
+  {
+    budget:'$50.000+',
+  },
+
+];
 
